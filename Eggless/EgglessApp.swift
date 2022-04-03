1dc067619bc82e746a5b46fa7c96b8433e0ab407
daryl <martinezdario123@gmail,.com>
2022-04-03 12:36:56 -0700

    Initial Commit

---
+++ Eggless/EgglessApp.swift
@@ -0,0 +1,17 @@
+//
+//  EgglessApp.swift
+//  Eggless
+//
+//  Created by daryl on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct EgglessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
